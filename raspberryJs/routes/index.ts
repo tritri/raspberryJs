23d0ac0a32a0c4e0fdb723912282c40289fd96d6
tritri <sakaitri>
2016-06-06 01:24:44 +0900

    firstcommit

---
+++ raspberryJs/routes/index.ts
@@ -0,0 +1,9 @@
+﻿import express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req: express.Request, res: express.Response, next: Function) {
+    res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
---
+++ raspberryJs/routes/users.ts
@@ -0,0 +1,9 @@
+﻿import express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req: express.Request, res: express.Response, next: Function) {
+    res.send('respond with a resource');
+});
+
+module.exports = router;
