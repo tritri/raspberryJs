952f76e8ca913ed04cffc1f2cd20bb674edc2a20
Tri <sakaitri@gmail.com>
2016-06-19 22:35:42 +0900

    モータードライバ導入

--- raspberryJs/routes/index.ts
+++ raspberryJs/routes/index.ts
@@ -1,4 +1,64 @@
 ﻿
+var DRV8830_0 = 0x60;
+var DRV8830_1 = 0x62;
+var CONTROL_REG = 0x00;
+var FAULT_REG = 0x01;
+var STANBY = 0x00;
+var NEG_ROT = 0x02;
+var POS_ROT = 0x01;
+var BREAK = 0x03;
+var express = require('express');
+var router = express.Router();
+var i2c = require('i2c');
+var addr = DRV8830_0;
+var wire0 = new i2c(DRV8830_0, { device: '/dev/i2c-1', debug: false });
+var wire1 = new i2c(DRV8830_1, { device: '/dev/i2c-1', debug: false });
+var motorDrive = (function () {
+    /**
+     * コンストラクタ
+     */
+    function motorDrive() {
+    }
+    /**
+     *  GET users listing.
+     *以下の感じでurlを入力すれば実行できる
+     *http://192.168.1.98:3000/users?num=0&drive=break&volt=1.0
+     * @param queryDatas
+     */
+    motorDrive.prototype.drive = function (voltage, motorNum, driveDir) {
+        var vSetF = voltage; //voltクエリにて電圧を取得
+        var vSet = ((vSetF * 100)) / 8;
+        var drive = STANBY;
+        var driveStr = driveDir; //driveクエリにて回転方向を取得
+        if (driveStr == 'pos') {
+            drive = POS_ROT;
+        }
+        else if (driveStr == 'neg') {
+            drive = NEG_ROT;
+        }
+        else if (driveStr == 'break') {
+            drive = BREAK;
+        }
+        else {
+        }
+        var controlData = CONTROL_REG;
+        var byteData = (vSet << 2) | drive;
+        console.log(byteData);
+        var motorNo = motorNum;
+        //var wire;
+        if (motorNo == 0) {
+            wire0.writeBytes(controlData, [byteData], function (err, res) { });
+        }
+        else {
+            wire1.writeBytes(controlData, [byteData], function (err, res) { });
+        }
+        //wire.writeBytes(0x16, [0x05], function(err, res){});
+        return true;
+    };
+    return motorDrive;
+}());
+
+
 import express = require('express');
 var router = express.Router();
 
@@ -13,7 +73,6 @@ router.get('/', function (req: express.Request, res: express.Response, next: Fun
 //formの属性、action="/"としないとここには飛んでこない
 router.post('/', function (req, res, next) {
     var str = req.body['input1'];
-
     
     //レスポンスはなぜか直接レスポンス先のjadeファイルを指定しないとだめ？
     res.render('layout',
@@ -29,8 +88,8 @@ router.post('/layout', function (req, res, next) {
     var str = req.query['buttonName'];
     
     /// <reference path="./motorDrive.ts"/>
-    //var motor= new motorDrive();
-    //motor.drive(2.0, 0, "pos");
+    var motor= new motorDrive();
+    motor.drive(2.0, 0, "pos");
 
     res.json(
         {
